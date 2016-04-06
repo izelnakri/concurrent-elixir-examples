1f1c9e1dec2dfce9fbe4aad2dad65fbc0baffa4e
Izel Nakri <contact@izelnakri.com>
2016-04-06 14:57:45 +0200

    Elixir agent example added

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 This spawns 1.000.000 elixir processes sends and receives data back with very little computation. It takes about 14 secs on my computer.
 
 ``` elixir --erl "+P 1000000" -r lib/chain.ex -e "Chain.run(1_000_000)" ```
+
+Agents - recursive function caching example on fibonacci function:
+
+``` elixir lib/spawn/fib_agent.exs ```
+
 ## Installation
 
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
--- lib/chain.ex
+++ lib/chain.ex
@@ -1,20 +1,21 @@
 defmodule Chain do
-    def counter(next_pid) do
+    def counter(pid) do
         receive do
             n ->
-                send next_pid, n + 1
+                IO.puts("n is #{n}")
+                send pid, n + 1
         end
     end
 
     def create_processes(n) do
         last = Enum.reduce 1..n, self,
-                fn (_, send_to) ->
-                    spawn(Chain, :counter, [send_to])
+                fn (_, pid) ->
+                    spawn(Chain, :counter, [pid])
                 end
         send last, 0
 
         receive do
-            final_answer when is_integer(final_answer) ->
+            final_answer ->
                 "Result is #{inspect(final_answer)}"
         end
     end
